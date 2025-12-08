8ed7e85f33cdc15da6de83fba14cbb63e6b65a6c
“[Muhaiminul <[muhaimin1471@gmail.com]>
2025-12-09 00:52:06 +0600

    ctuu mau price updated

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,5 @@
 const products = [
   { id: 1, name: "Pocha mauu", desc: "Pocha mauu, kintu valo", price: 300, img: "assets/images/pocha mau.jpg" },
   { id: 2, name: "Valo mauuu", desc: "Valo mauu kintu mota", price: 350, img: "assets/images/valo mau.JPG" },
-  { id: 3, name: "Cutu mauuu", desc: "Cutu mauu", price: 325, img: "assets/images/cutu mau.jpg" }
+  { id: 3, name: "Cutu mauuu", desc: "Cutu mauu", price: 425, img: "assets/images/cutu mau.jpg" }
 ];
