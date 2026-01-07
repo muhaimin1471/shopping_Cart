e7f16693ba880883d5a42a3b274df1614ea26e9a
“[Muhaiminul <[muhaimin1471@gmail.com]>
2026-01-07 18:42:09 +0600

    Promo code added

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,5 @@
 const products = [
   { id: 1, name: "Apple", desc: "Fresh apples keeps doctors away", price: 300, img: "assets/images/apple.jpg" },
-  { id: 2, name: "Banana", desc: "Keeps you healty and strong", price: 150, img: "assets/images/banana.JPG" },
+  { id: 2, name: "Banana", desc: "Keeps you healty and strong", price: 150, img: "assets/images/banana.jpg" },
   { id: 3, name: "Potato", desc: "Plain round potato", price: 30, img: "assets/images/potato.jpg" }
 ];
